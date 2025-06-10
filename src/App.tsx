65caa46fa96f4a6c1bb51f82816b5541009bae70
김근영 <kkylsi@naver.com>
2025-06-10 11:37:49 +0900

    project init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 text-2xl font-bold">
+      Crowdworks 프로젝트 시작!
+    </div>
+  )
+}
+
+export default App
